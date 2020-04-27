3ee641731cedefd212f685d5e85131394d22d6ee
scrapehero-code <scrapehero@gmail.com>
2020-04-28 03:02:57 +0530

    Remove \n from urls.txt when reading lines.

--- amazon.py
+++ amazon.py
@@ -16,9 +16,6 @@ def scrape(url):
         'upgrade-insecure-requests': '1',
         'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'none',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-dest': 'document',
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
     }
 
@@ -37,7 +34,7 @@ def scrape(url):
 
 # product_data = []
 with open("urls.txt",'r') as urllist, open('output.jsonl','w') as outfile:
-    for url in urllist.readlines():
+    for url in urllist.read().splitlines():
         data = scrape(url) 
         if data:
             json.dump(data,outfile)
